c0b5773a54f2014855f9d61a86e50a83fda0f05d
Doug Wilson <d@dkw.io>
2023-10-16 14:39:22 -0700

    Create debian-ubunto-setup.sh
    Exactly what it looks like.

---
+++ debian-ubunto-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Add the release PGP keys:
+sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+
+# Add the "stable" channel to your APT sources:
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+# Update and install syncthing:
+sudo apt-get update
+sudo apt-get install syncthing
+
+# Increase preference of Syncthing's packages ("pinning")
+printf "Package: *\nPin: origin apt.syncthing.net\nPin-Priority: 990\n" | sudo tee /etc/apt/preferences.d/syncthing
