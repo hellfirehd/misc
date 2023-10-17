eb9b0be7fadb75c090b959fe51170db84d3e23bc
Doug Wilson <d@dkw.io>
2023-10-17 10:26:55 -0700

    Update debian-ubunto-setup.sh
    Add .NET 6/7 SDKs

--- debian-ubunto-setup.sh
+++ debian-ubunto-setup.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+### Syncthing
 # Add the release PGP keys:
 sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
-
 # Add the "stable" channel to your APT sources:
 echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
-
-# Update and install syncthing:
+# Update
 sudo apt-get update
-sudo apt-get install syncthing
-
 # Increase preference of Syncthing's packages ("pinning")
 printf "Package: *\nPin: origin apt.syncthing.net\nPin-Priority: 990\n" | sudo tee /etc/apt/preferences.d/syncthing
+# Install syncthing:
+sudo apt-get install syncthing
+
+### .NET SDK
+# Get Ubuntu version
+declare repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
+# Download Microsoft signing key and repository
+wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+# Install Microsoft signing key and repository
+sudo dpkg -i packages-microsoft-prod.deb
+# Clean up
+rm packages-microsoft-prod.deb
+# Update packages
+sudo apt update
+sudo apt install dotnet-sdk-6.0 dotnet-sdk-7.0
